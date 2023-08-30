959dca9b90956190e4388669884581617891e56f
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-08-31 04:31:09 +0600

    Add files via upload

---
+++ Basic-Java/java01_introduction.java
@@ -0,0 +1,12 @@
+
+public class java01_introduction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello world, my name is noushad ");
+    
+    }
+    
+}
