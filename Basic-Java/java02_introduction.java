510dff85bb8ccbbc282748aac27269b7fc23b731
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-08-31 04:31:42 +0600

    Add files via upload

---
+++ Basic-Java/java02_introduction.java
@@ -0,0 +1,13 @@
+
+public class java02_introduction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        System.out.println("MY Computer is Acer");
+    
+    }
+    
+}
