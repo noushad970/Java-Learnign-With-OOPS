631924fcf4cf2aa586533fb361e7f7499c854df5
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-01 01:57:41 +0600

    Add files via upload

---
+++ Basic-Java/java03_Variables.java
@@ -0,0 +1,25 @@
+public class java03_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+      
+        String Name="Noushad";
+        System.out.println(Name);
+        int $sumit=100;
+        float money=100.22f;
+        boolean isWalk=false;
+        double earn=10000.33;
+        char a='N';
+        System.out.println($sumit);
+        System.out.println(money);
+        System.out.println(isWalk);
+        System.out.println(earn);
+        System.out.println(a);
+        System.out.println(money+" is My Earning, My name is "+Name);
+    
+    }
+    
+}
