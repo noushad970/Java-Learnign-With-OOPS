cd1eec064f41501c261d91b5f285bf03d11883a8
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-02 02:53:49 +0600

    Add files via upload

---
+++ Basic-Java/java07_math_function.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class java07_math_function {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+      int num1=10,num2=112,num3=1;
+      System.out.println(Math.max(num1, num3));
+      System.out.println(Math.min(num2, num3));
+      System.out.println(Math.sqrt(112));
+      System.out.println(Math.abs(-121));
+      System.out.println(Math.random());
+      System.out.println(4+(8-4)*Math.random());
+      System.out.println(4+(8-4)*Math.random());
+      System.out.println(4+(8-4)*Math.random());
+      System.out.println(4+(8-4)*Math.random());
+
+      
+       
+        
+       
+
+
+
+    }
+    
+}
