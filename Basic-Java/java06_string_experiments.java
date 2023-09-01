0ad63c5b16bff599c5a34c0492fcee2067ce4f65
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-02 02:53:23 +0600

    Add files via upload

---
+++ Basic-Java/java06_string_experiments.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class java06_string_experiments {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+      
+       String name="Noushad";
+       System.out.println(name.length());
+       System.out.println(name.toLowerCase());
+       System.out.println(name.toUpperCase());
+       System.out.println(name.contains("NOU"));
+       System.out.println(name.charAt(3));
+       System.out.println(name.endsWith("ad"));
+       System.out.println(name.endsWith("AD"));
+       System.out.println(name.indexOf("S"));
+       
+       
+        
+       
+
+
+
+    }
+    
+}
