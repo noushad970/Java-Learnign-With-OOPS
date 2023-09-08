b9f73b2dcd262a70bdab3b48b0b0050caff1cd4d
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-09 04:38:11 +0600

    Add files via upload

---
+++ OOP Concepts/07 overloadingConstructor/src/returnedPeremeter.java
@@ -0,0 +1,12 @@
+public class returnedPeremeter {
+
+    int square(int n)
+    {
+        return n*n;
+    }
+    int sum(int n)
+    {
+        return n+n;
+    }
+    
+}
---
+++ OOP Concepts/07 overloadingConstructor/src/test2.java
@@ -0,0 +1,9 @@
+public class test2 {
+    public static void main(String[] args) {
+        returnedPeremeter n1=new returnedPeremeter();
+        int n=n1.square(34);
+        System.out.println("The square value of n is: "+n);
+        System.out.println("The sum of n and n is: "+n1.sum(34));
+    }
+    
+}
