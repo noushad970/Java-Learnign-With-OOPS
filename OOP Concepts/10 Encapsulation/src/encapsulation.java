1e0bc9e3f3d40fbf047baeceb2838b586cfb2173
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-11 03:16:52 +0600

    Add files via upload

---
+++ OOP Concepts/10 Encapsulation/src/encapsulation.java
@@ -0,0 +1,30 @@
+public class encapsulation{
+    private int age;
+    private String name;
+    private int id;
+    public int geta()
+    {
+        return age;
+    }
+    public void seta(int age)
+    {
+        this.age=age;
+    }
+    public String set()
+    {
+        return name;
+    }
+    public void get(String name)
+    {
+        this.name=name;
+    }
+    public int SetId()
+    {
+        return id;
+    }
+    public void getId(int id)
+    {
+        this.id=id;
+    }
+
+}
---
+++ OOP Concepts/10 Encapsulation/src/test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+    public static void main(String[] args) {
+        encapsulation en=new encapsulation();
+        en.get("Noushad");
+        System.out.println(en.set());
+        en.getId(219332);
+        System.out.println(en.SetId());
+        en.seta(21);
+        System.out.println(en.geta());
+    }
+    
+}
