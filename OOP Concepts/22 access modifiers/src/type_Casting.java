72188e5d498cefb5d7c7a9f1f7c71eaa2cb1932e
MD. NOUSHAD BHUIYAN <142130428+noushad970@users.noreply.github.com>
2023-09-24 00:48:22 +0600

    Add files via upload

---
+++ OOP Concepts/22 access modifiers/src/type_Casting.java
@@ -0,0 +1,19 @@
+public class type_Casting {
+    public static void main(String[] args) {
+        //implisive datatype:
+        int x=57;
+        double y=x;
+        System.out.println(y);
+        //explicit datatype
+        double d=10.4;
+        int i= (int)d;
+        System.out.println(i);
+        char z=(char)x;
+        System.out.println(z);
+    }
+}
+/*
+by typecasting we can change datatype of a variable.
+there are two type of typecasting inplisive typecasting and explicit datatype
+
+ */
