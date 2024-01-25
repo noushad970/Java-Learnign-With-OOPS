52d7eef755d450bc033d00d4678870df755734d2
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2024-01-26 05:56:22 +0600

    Add files via upload

---
+++ OOP Concepts/Abstraction/problem 06/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Area areaCalculator = new Area();
+
+        areaCalculator.RectangleArea(5.0, 8.0);
+        areaCalculator.SquareArea(4.5);
+        areaCalculator.CircleArea(3.0);
+    }
+}
